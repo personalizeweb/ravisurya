23184f581ff6b4b21860991097387f2404235fdb
web <110479064+personalizeweb@users.noreply.github.com>
2023-06-21 19:57:36 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$('#GetFile').on('click', function () {
+    $.ajax({
+        url: './sample.pdf',
+        method: 'GET',
+        xhrFields: {
+            responseType: 'blob'
+        },
+        success: function (data) {
+            var a = document.createElement('a');
+            var url = window.URL.createObjectURL(data);
+            a.href = url;
+            a.download = 'myfile.pdf';
+            document.body.append(a);
+            a.click();
+            a.remove();
+            window.URL.revokeObjectURL(url);
+        }
+    });
+});
